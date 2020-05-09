ade89b4a35c515d0b580aded37d0482caa3dce04
DanilSupreme <59294947+CeRBeR-DeV@users.noreply.github.com>
2020-05-09 23:37:05 +0300

    DMMT

--- Source.cpp
+++ Source.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 
-template<typename T>void fill_rand(T arr[], T& n);
-template<typename T>void print_array(T arr[], T& n);
-template<typename T>void push_back(T* &arr, T& n, T value);
-template<typename T>void push_front(T* &arr, T& n, T value);
-template<typename T>void insert(T* &arr, T& n, int index, T value);
-template<typename T>void pop_back(T* &arr, T& n);
-template<typename T>void pop_front(T* &arr, T& n);
-template<typename T>void erase(T* &arr, T& n, int index);
+template<typename T>void fill_rand(T arr[], T n);
+template<typename T>void print_array(T arr[], T n);
+template<typename T>T* push_back(T arr[], T& n, T value);
+template<typename T>T* push_front(T arr[], T& n, T value);
+template<typename T>T* insert(T arr[], T& n, int index, T value);
+template<typename T>T* pop_back(T arr[], T& n);
+template<typename T>T* pop_front(T arr[], T& n);
+template<typename T>T* erase(T arr[], T& n, int index);
 
 void main()
 {
@@ -22,7 +22,7 @@ void main()
 	std::cout << std::endl;
 	std::cout << "¬ведите значение которое будет добавлено в конец массива: ";
 	std::cin >> value;
-	push_back(arr, n, value);
+	arr=push_back(arr, n, value);
 	std::cout << "»сходный массив: ";
 	print_array(arr, n);
 	std::cout << std::endl;
@@ -36,13 +36,13 @@ void main()
 	std::cin >> index;
 	std::cout << "¬ведите значение: ";
 	std::cin >> value;
-	insert(arr, n, value, index);
+	arr=insert(arr, n, value, index);
 	std::cout << "»сходный массив: ";
 	print_array(arr, n);
 	std::cout << std::endl;
 	std::cout << "”даление последнего елемента в массиве:";
 	std::cout << std::endl;
-	pop_back(arr, n);
+	arr=pop_back(arr, n);
 	std::cout << "»сходный массив: ";
 	print_array(arr, n);
 	std::cout << std::endl;
@@ -54,20 +54,20 @@ void main()
 	std::cout << std::endl;
 	std::cout << "¬ведите номер €чейки которую нужно удалить из массива:";
 	std::cin >> index;
-	erase(arr, n, index);
+	arr = erase(arr, n, index);
 	std::cout << "»сходный массив:";
 	print_array(arr, n);
 	delete[] arr;
 }
-template<typename T>void fill_rand(T arr[], T& n)
+template<typename T>void fill_rand(T arr[], T n)
 {
 	for (T i = 0; i < n; i++)arr[i] = rand() % 100;
 }
-template<typename T>void print_array(T arr[], T& n)
+template<typename T>void print_array(T arr[], T n)
 {
 	for (T i = 0; i < n; i++)std::cout << arr[i] << "\t";
 }
-template<typename T>void push_back(T* &arr, T& n, T value)
+template<typename T>T* push_back(T arr[], T& n, T value)
 {
 	T* buffer = new T[n + 1];
 	for (T i = 0; i < n; i++)buffer[i] = arr[i];
@@ -75,8 +75,9 @@ template<typename T>void push_back(T* &arr, T& n, T value)
 	arr = buffer;
 	arr[n] = value;
 	n++;
+	return arr;
 }
-template<typename T>void push_front(T* &arr, T& n, T value)
+template<typename T>T* push_front(T arr[], T& n, T value)
 {
 	T* buffer = new T[n + 1];
 	for (T i = 0; i < n; i++)buffer[i + 1] = arr[i];
@@ -84,8 +85,9 @@ template<typename T>void push_front(T* &arr, T& n, T value)
 	arr = buffer;
 	arr[0] = value;
 	n++;
+	return arr;
 }
-template<typename T>void insert(T* &arr, T& n, int index, T value)
+template<typename T>T* insert(T arr[], T& n, int index, T value)
 {
 	T* buffer = new T[n + 1];
 	for (T i = 0; i < index; i++)buffer[i] = arr[i];
@@ -94,24 +96,27 @@ template<typename T>void insert(T* &arr, T& n, int index, T value)
 	delete[] arr;
 	arr = buffer;
 	n++;
+	return arr;
 }
-template<typename T>void pop_back(T* &arr, T& n)
+template<typename T>T* pop_back(T arr[], T& n)
 {
 	T* buffer = new T[n];
 	for (T i = 0; i < n; i++)buffer[i] = arr[i];
 	delete[] arr;
 	arr = buffer;
 	n--;
+	return arr;
 }
-template<typename T>void pop_front(T* &arr, T& n)
+template<typename T>T* pop_front(T arr[], T& n)
 {
 	T* buffer = new T[n];
 	for (T i = 0; i < n; i++)buffer[i] = arr[i + 1];
 	delete[] arr;
 	arr = buffer;
 	n--;
+	return arr;
 }
-template<typename T>void erase(T* &arr, T& n, int index)
+template<typename T>T* erase(T arr[], T& n, int index)
 {
 	T* buffer = new T[n];
 	for (T i = 0; i < index; i++)buffer[i] = arr[i];
@@ -119,4 +124,5 @@ template<typename T>void erase(T* &arr, T& n, int index)
 	delete[] arr;
 	arr = buffer;
 	n--;
+	return arr;
 }
